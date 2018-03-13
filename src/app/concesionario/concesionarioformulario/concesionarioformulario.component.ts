3a66720523a572597317ee5811053dd4f4720c33
joseba10 <joseba10>
2018-03-13 20:11:28 +0100

    Casi Finalizado + Imagenes

--- src/app/concesionario/concesionarioformulario/concesionarioformulario.component.ts
+++ src/app/concesionario/concesionarioformulario/concesionarioformulario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Coche } from '../../model/coche';
+import { CochesService } from '../../providers/coches.service';
 
 @Component({
   selector: 'app-concesionarioformulario',
@@ -6,10 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./concesionarioformulario.component.scss']
 })
 export class ConcesionarioformularioComponent implements OnInit {
+  coche: Coche;
+  formulario:FormGroup;
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder, private cochesService : CochesService) { 
+
+    console.log('FormComponent constructor');
+    this.createForm(); 
+  }
 
   ngOnInit() {
+  
+    console.log('FormComponent ngOnInit');
+  
   }
+  createForm(){
+    console.log('FormComponent createForm');
+    this.formulario = this.fb.group({
+      marca : ['', [Validators.required,Validators.minLength(4)] ],
+      modelo : ['',[Validators.required,Validators.minLength(2)] ],
+      version:  '',
+      foto: 'assets/imgs/coche_default.jpg',
+      puertas :  0,
+      caballos : 0,
+      consumo :  5
+    });
+}
+
+onSubmit(event){
+  console.log('FormComponent onSubmit formulario %o' , this.formulario.value );
+
+  //Crear Coche a partir del formulario
+  this.coche = this.mapperFormToCoche(this.formulario);
+
+  //limpiar formulario
+  this.formulario.reset();
+
+
+  console.log('Llamar Servicio para crear Coche');
+  this.cochesService.put(this.coche);
+
+
+}
+
+private mapperFormToCoche( form : FormGroup ):Coche{
+  let coche = new Coche( 
+    form.value.marca ,
+    form.value.modelo,
+    form.value.puertas,
+    form.value.foto,
+    form.value.version
+  );
+  coche.caballos = form.value.caballos;
+  coche.consumo = form.value.consumo;
+  return coche;
+}
 
 }
--- src/app/providers/coches.service.ts
+++ src/app/providers/coches.service.ts
@@ -5,8 +5,10 @@ import { element } from 'protractor';
 
 @Injectable()
 export class CochesService {
-
+  
+coches:Coche[];
   constructor() { 
+    this.coches = [];
     console.log('CochesService constructor');
   }
 
@@ -15,7 +17,7 @@ export class CochesService {
   */
   getAll():Coche[]{
     console.log('CochesService getAll');
-    let coches:Coche[] = [];
+    this.coches = [];
     let coche;
     
     let jsonData = JSON.parse(MOCKS_COCHES.stock);
@@ -32,11 +34,11 @@ export class CochesService {
                           element.version
                           );
 
-        coches.push(coche);
+                          this.coches.push(coche);
 
     });
 
-    return coches;
+    return this.coches;
   }
 
   /**
@@ -49,6 +51,22 @@ export class CochesService {
 //this.coches.unshift(Coche);
 
   }
+
+  
+  put( coche:Coche){
+    console.log('CochesService put');    
+    this.coches.unshift(coche);
+
+        /*
+                    add  remove  start  end
+            push    X                   X
+            pop           X            X
+        unshift    X             X
+          shift           X      X
+          
+        */
+
+}
 
 
 }
